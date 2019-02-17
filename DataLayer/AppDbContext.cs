ed01b7bebbcfe734c0b495f89bf09175ebf7c86a
ECR\Egon Rasmussen <ecr@live.dk>
2019-02-17 11:09:11 +0100

    ok

---
+++ DataLayer/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+    }
+
+}
