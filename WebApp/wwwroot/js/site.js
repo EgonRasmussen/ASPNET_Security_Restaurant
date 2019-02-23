5ffdf5be0361fa40a40373258b8e0b78d8645ffa
ECR\Egon Rasmussen <ecr@live.dk>
2019-02-23 10:27:38 +0100

    ok

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -1,4 +1,15 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿(function () {
+    var defaultOptions = {
+        highlight: function (element) {
+            var $formGroup = $(element).closest(".form-group");
+            $formGroup.find(".form-control").addClass('is-invalid');
+        },
+        unhighlight: function (element) {
+            var $formGroup = $(element).closest(".form-group");
+            $formGroup.find(".form-control").removeClass('is-invalid');
+            $formGroup.find(".form-control").addClass('is-valid');
+        }
+    };
 
-// Write your Javascript code.
+    $.validator.setDefaults(defaultOptions);
+})();
